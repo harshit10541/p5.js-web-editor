030e8c68f17d077265972a7a8910c7379ef8a222
Harshit <harshit10541@gmail.com>
2024-11-01 09:36:35 +0530

    resolved the repeated code issue between two files

--- client/modules/Preview/EmbedFrame.jsx
+++ client/modules/Preview/EmbedFrame.jsx
@@ -7,8 +7,8 @@ import { JSHINT } from 'jshint';
 import decomment from 'decomment';
 import { resolvePathToFile } from '../../../server/utils/filePath';
 import getConfig from '../../utils/getConfig';
+import resolvePathsForElementsWithAttribute from '../../../server/utils/filePath';
 import {
-  MEDIA_FILE_REGEX,
   MEDIA_FILE_QUOTED_REGEX,
   STRING_REGEX,
   PLAINTEXT_FILE_REGEX,
@@ -33,19 +33,6 @@ const Frame = styled.iframe`
     position: relative;
   `}
 `;
-
-function resolvePathsForElementsWithAttribute(attr, sketchDoc, files) {
-  const elements = sketchDoc.querySelectorAll(`[${attr}]`);
-  const elementsArray = Array.prototype.slice.call(elements);
-  elementsArray.forEach((element) => {
-    if (element.getAttribute(attr).match(MEDIA_FILE_REGEX)) {
-      const resolvedFile = resolvePathToFile(element.getAttribute(attr), files);
-      if (resolvedFile && resolvedFile.url) {
-        element.setAttribute(attr, resolvedFile.url);
-      }
-    }
-  });
-}
 
 function resolveCSSLinksInString(content, files) {
   let newContent = content;
